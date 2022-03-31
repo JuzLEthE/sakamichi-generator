a67899980a1716266e090d0b1c047db42a3cee29
JuzLEthE <1607067156@qq.com>
2022-03-31 20:39:35 +0800

    预览版更新内容

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -28,17 +28,13 @@
       <div class="module5" onclick="window.open('https://preview.hnz46.cn')">
         <div class="description">
           <h2>预览版</h2>
-          <h4>2022年3月9日</h4>
-          <p>1.更新html2canvas版本至1.4.1。</p>
-          <p>2.更新音频样式。</p>
+          <h4>2022年3月31日</h4>
+          <p>1.更新日向坂七单「僕なんか」公式照。</p>
           <p>
             <br />
           </p>
           <h4>已同步到发布版的内容:</h4>
-          <p>1.调整部分图标样式。</p>
-          <p>2.调整乃木坂音频消息样式。</p>
-          <p>3.调整消息时间格式为:M/dd hh:mm。</p>
-          <p>4.调整生成图片文件名为:成员名_MMdd.png。</p>
+          <p>1.全部。</p>
         </div>
       </div>
     </div>
