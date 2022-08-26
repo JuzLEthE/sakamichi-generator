f5aaa748011c2c3a3dcbce66e8d7519ecee8effe
JuzLEthE <1607067156@qq.com>
2022-08-26 19:56:46 +0800

    备案问题修复

--- src/router/index.js
+++ src/router/index.js
@@ -11,7 +11,7 @@ const routes = [
     name: 'Home',
     component: Home,
     meta: {
-      title: 'msg图制作工具'
+      title: '个人数据分析'
     }
   },
   {
@@ -46,7 +46,7 @@ const mapping = { hinata: '日向坂46', sakura: '櫻坂46', nogi: '乃木坂46'
 router.beforeEach((to, from, next) => {
   let groupName = mapping[to.params.group]
 
-  document.title = (groupName ? groupName : '坂道') + to.meta?.title
+  document.title = (groupName ? groupName : '') + to.meta?.title
   next()
 })
 
